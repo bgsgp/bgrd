372c2dab3ee913cf7929c72736cf258180a02991
bgsgp <beggarsgroup.lyy@gmail.com>
2026-05-09 13:15:39 +0800

    完成5.3更改提交。

--- asset_manager.py
+++ asset_manager.py
@@ -4,9 +4,10 @@ from typing import Optional
 
 class AssetManager:
     def resource_path(self, relative_path: str) -> str:
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
+        # 判断是否为打包后的可执行环境
+        if getattr(sys, 'frozen', False):
+            base_path = os.path.dirname(sys.executable)
+        else:
             base_path = os.path.abspath(".")
         return os.path.join(base_path, relative_path)
 
